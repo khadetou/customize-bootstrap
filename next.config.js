11f90b4a6b35c4a5378d478878282bc695f23f8b
khadetou <khadetou96@gmail.com>
2021-11-26 14:57:19 +0000

    next-config sass

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withSass = require("@zeit/next-sass");
+
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    camelCase: "dashes",
+    importLoaders: 1,
+    localIndentName: "[local]___[hash:base64:5]",
+  },
+  sassLoaderOptions: {
+    includePaths: ["node_modules", "./node_modules"],
+  },
   reactStrictMode: true,
-}
+});
